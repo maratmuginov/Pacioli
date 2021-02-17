ddc252567b3ea99922e7fc8978822209b57fe6b5
itian <kristianne.nerona@gmail.com>
2021-02-16 20:09:17 -0800

    Updated code in JournalEntry_Throws_Exception_On_Empty_Credit_Or_Debit

--- source/Pacioli/Pacioli.Tests/Tests/JournalTests.cs
+++ source/Pacioli/Pacioli.Tests/Tests/JournalTests.cs
@@ -25,7 +25,8 @@ namespace Pacioli.Tests.Tests
         [Theory, ClassData(typeof(JournalEntryHasToHaveCreditAndDebit_TestData))]
         public void JournalEntry_Throws_Exception_On_Empty_Credit_Or_Debit(List<JournalEntryItem> debits, List<JournalEntryItem> credits)
         {
-            Assert.ThrowsAny<Exception>(() => new JournalEntry(debits: debits, credits: credits));
+            Action sut = () => new JournalEntry(debits: debits, credits: credits);
+            Assert.ThrowsAny<Exception>(sut);
         }
 
         private record JournalEntryHasToHaveCreditAndDebit_TestData : IEnumerable<object[]>
