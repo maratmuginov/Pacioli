81db97434045c571ea31fa68f90e0fd8127d53e0
itian <kristianne.nerona@gmail.com>
2021-02-16 19:29:34 -0800

    Added test: if JournalItems arguments are empty or null, it should throw an exception

--- source/Pacioli/Pacioli.Lib/Models/JournalEntry.cs
+++ source/Pacioli/Pacioli.Lib/Models/JournalEntry.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System;
 
 namespace Pacioli.Lib.Models
 {
-    public class JournalEntry
+    public record JournalEntry
     {
         public JournalEntry()
         {
             Debits = new List<JournalEntryItem>();
             Credits = new List<JournalEntryItem>();
+        }
+
+        public JournalEntry([NotNull] List<JournalEntryItem> debits, [NotNull] List<JournalEntryItem> credits)
+        {
+            if ((!debits?.Any() ?? false) || (!credits?.Any() ?? false))
+                throw new ArgumentException("Debits and credits length should not be empty.");
         }
 
         public List<JournalEntryItem> Debits { get; set; }
