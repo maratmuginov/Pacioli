da5a0f8c830812cdef23a19863005d58399aed36
itian <kristianne.nerona@gmail.com>
2021-02-22 13:03:06 -0800

    Made the mocked userManager set up shorter by splitting to two logical methods; more edits to the test for a planned future test.

--- source/Pacioli/Pacioli.Tests/Tests/AuthorizationAndAuthenticationTests.cs
+++ source/Pacioli/Pacioli.Tests/Tests/AuthorizationAndAuthenticationTests.cs
@@ -1,16 +1,19 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.Extensions.Configuration;
 using Moq;
+using Pacioli.WebApi;
 using Pacioli.WebApi.Controllers;
 using Pacioli.WebApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Pacioli.Tests.Tests
 {
-    public class AuthorizationAndAuthenticationTests
+    public class AuthorizationAndAuthenticationTests : IClassFixture<WebApplicationFactory<Startup>>
     {
         private readonly Mock<IConfiguration> _configuration;
 
@@ -25,7 +28,7 @@ namespace Pacioli.Tests.Tests
         [Fact]
         public async Task AuthorizeAccountantWithCorrectCredentials()
         {
-            UserController sut = new(MockUserManager<Accountant>(users).Object, _configuration.Object);
+            UserController sut = new(CreateFakeUserManager().Object, _configuration.Object);
 
             IActionResult result = await sut.Login(new LoginModel { Email = "fakeEmail@fakes.com", Password = "fakePassword" });
 
@@ -35,36 +38,45 @@ namespace Pacioli.Tests.Tests
         [Fact]
         public async Task RejectAccountantWithInCorrectCredentials()
         {
-            UserController sut = new(MockUserManager<Accountant>(users, false).Object, _configuration.Object);
+            UserController sut = new(CreateFakeUserManager(false).Object, _configuration.Object);
 
             IActionResult result = await sut.Login(new LoginModel { Email = "fakeEmail@fakes.com", Password = "fakePassword" });
 
             Assert.IsType<UnauthorizedResult>(result);
         }
 
-        public static Mock<UserManager<Accountant>> MockUserManager<TUser>(List<TUser> fakeUserStore, bool isValidUser = true)
+        public static Mock<UserManager<IdentityUser>> CreateFakeUserManager(bool authorizeLogin = true)
         {
-            Mock<IUserStore<Accountant>> store = new();
-            Mock<UserManager<Accountant>> mockedUserManager = new(store.Object, null, null, null, null, null, null, null, null);
-            mockedUserManager.Object.UserValidators.Add(new UserValidator<Accountant>());
-            mockedUserManager.Object.PasswordValidators.Add(new PasswordValidator<Accountant>());
-            mockedUserManager.Setup(m => m.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new Accountant { Email = "fakeUser3@fakes.com" });
-            mockedUserManager.Setup(m => m.CheckPasswordAsync(It.IsAny<Accountant>(), It.IsAny<string>())).ReturnsAsync(isValidUser);
-            mockedUserManager.Setup(m => m.GetRolesAsync(It.IsAny<Accountant>())).ReturnsAsync(new List<string> { "Accountant" });
+            Mock<IUserStore<IdentityUser>> store = new();
+            Mock<UserManager<IdentityUser>> mockedUserManager = new(store.Object, null, null, null, null, null, null, null, null);
+            IdentityUser fakeUser = new() { Email = "fakeUser3@fakes.com" };
+            mockedUserManager.Setup(m => m.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(fakeUser);
+            mockedUserManager.Setup(m => m.CheckPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>())).ReturnsAsync(authorizeLogin);
+            mockedUserManager.Setup(m => m.GetRolesAsync(It.IsAny<IdentityUser>())).ReturnsAsync(new List<string> { "Accountant" });
+            return mockedUserManager;
+        }
 
-            mockedUserManager.Setup(x => x.DeleteAsync(It.IsAny<Accountant>())).ReturnsAsync(IdentityResult.Success);
-            mockedUserManager.Setup(x => x.CreateAsync(It.IsAny<Accountant>(), It.IsAny<string>()))
+        public static Mock<UserManager<IdentityUser>> DecorateFakeUserManagerWithFakeUserCRUDMethods(Mock<UserManager<IdentityUser>> mockedUserManager, List<IdentityUser> fakeUserStore)
+        {
+            mockedUserManager.Setup(x => x.DeleteAsync(It.IsAny<IdentityUser>())).ReturnsAsync(IdentityResult.Success);
+            mockedUserManager.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), It.IsAny<string>()))
                 .ReturnsAsync(IdentityResult.Success)
-                .Callback<TUser, string>((x, y) => fakeUserStore.Add(x));
-            mockedUserManager.Setup(x => x.UpdateAsync(It.IsAny<Accountant>())).ReturnsAsync(IdentityResult.Success);
+                .Callback<IdentityUser, string>((x, y) => fakeUserStore.Add(x));
+            mockedUserManager.Setup(x => x.UpdateAsync(It.IsAny<IdentityUser>())).ReturnsAsync(IdentityResult.Success);
 
             return mockedUserManager;
         }
 
-        private List<Accountant> users = new List<Accountant>
+        private List<IdentityUser> FakeIdentityUsers = new()
         {
-            new Accountant { Email = "fakeUser1@fakes.com" },
-            new Accountant { Email = "fakeUser2@fakes.com" }
+            new IdentityUser { Email = "fakeUser1@fakes.com" },
+            new IdentityUser { Email = "fakeUser2@fakes.com" }
         };
+
+        //[Fact]
+        public async Task UserCanGoToMainPageAfterLogin()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
