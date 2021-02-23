155c2ffa6ee1c96f3a359d36dfbb3b44f4e6b230
maratmuginov <mmuginov@protonmail.com>
2021-02-23 10:02:53 -0800

    Split off Identity Lib

---
+++ source/Pacioli/Pacioli.Lib.Identity/Data/UserIdentityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Pacioli.Lib.Identity.Models;
+
+namespace Pacioli.Lib.Identity.Data
+{
+    public class UserIdentityDbContext : IdentityDbContext<User>
+    {
+        public UserIdentityDbContext(DbContextOptions<UserIdentityDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
