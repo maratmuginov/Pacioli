7bd1530e0837302d7a9d079dff468e35603437db
maratmuginov <mmuginov@protonmail.com>
2021-02-26 19:04:32 -0800

    Registration

---
+++ source/Pacioli/Pacioli.WebApi/Models/RegisterModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pacioli.WebApi.Models
+{
+    public readonly struct RegisterModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; init; }
+        [Required]
+        public string Username { get; init; }
+        [Required]
+        public string[] Roles { get; init; }
+        [Required]
+        public string Password { get; init; }
+    }
+}
