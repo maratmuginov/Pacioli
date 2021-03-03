6342d687892f0cc0bd6da331fe4ecc39b8f17ca6
maratmuginov <mmuginov@protonmail.com>
2021-03-03 12:05:24 -0800

    Shared lib

---
+++ source/Pacioli/Pacioli.Lib.Shared/Helpers/JsonHelper.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Pacioli.Lib.Shared.Helpers
+{
+    public static class JsonHelper
+    {
+        public static async Task<T> DeserializeFileAsync<T>(string filePath)
+        {
+            await using var stream = File.OpenRead(filePath);
+            return await JsonSerializer.DeserializeAsync<T>(stream);
+        }
+    }
+}
